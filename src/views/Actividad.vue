a2dc22c95c17d148a1b80b133c8ef4b61df1a0a1
deividramirezarias <deivid.innovatec@gmail.com>
2022-10-28 16:17:42 -0500

    actividad vinculada
    actividad vinculada

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -24,7 +24,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar y soltar
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
